6b8bc512ef44e47219ee4873be155a365bff0e86
Gabriel Dantas <gabriel.dantas@rivendel.com.br>
2019-07-29 18:17:10 -0300

    feat: Updated variables lb_tg module

--- load_balancer/lb_target_group/variables.tf
+++ load_balancer/lb_target_group/variables.tf
@@ -7,45 +7,47 @@ variable "target_group_port" {
 }
 
 variable "target_group_protocol" {
-  description = "[OPTIONAL] - "
+  default = "HTTP"
+  description = "[OPTIONAL] - The protocol to use for routing traffic to the targets"
 }
 
 variable "target_group_type" {
-  description = ""
-  
+  description = "[REQUIRED] - The type of target that you must specify when registering targets with this target group."
 }
 
 variable "target_group_vpc_id" {
-  description = ""
+  default = ""
+  description = "[OPTIONAL] - The identifier of the VPC in which to create the target group."
 }
 
 variable "target_group_tags" {
-  description = ""
+  default = {}
+  description = "[OPTIONAL] - A mapping of tags to assign to the target group."
 }
 
 variable "health_check_enabled" {
-  description = ""
+  description = "[REQUIRED] -"
 }
 variable "health_check_healthy_threshold" {
-  description = ""
+  description = "[REQUIRED] -"
 }
 variable "health_check_unhealthy_threshold" {
-  description = ""
+  description = "[REQUIRED] -"
 }
 
 variable "health_check_timeout" {
-  description = ""
+  description = "[REQUIRED] -"
 }
 variable "health_check_interval" {
-  description = ""
+  description = "[REQUIRED] -"
 }
 variable "health_check_path" {
-  description = ""
+  description = "[REQUIRED] -"
 }
 variable "health_check_port" {
-  description = ""
+  description = "[REQUIRED] -"
 }
 
 variable "health_check_matcher" {
-  description = ""
+  description = "[REQUIRED] -"
 }
